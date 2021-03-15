16f07a6fe8f9711651297b5a40ca627c568330ac
chrisowen <chrisowen491@gmail.com>
2021-03-15 20:27:26 +0000

    update

--- src/TranmereWebPlayerSearch.js
+++ src/TranmereWebPlayerSearch.js
@@ -46,6 +46,15 @@ exports.handler = async function (event, context) {
     for(var x=0; x < results.length; x++ ) {
         delete results[x].TimeToLive;
         results[x].bio = playerHash[results[x].Player];
+        if(results[x].bio.picLink) {
+            results[x].bio.pic = {
+               fields:{
+                   file:{
+                       url: app.bio.picLink
+                   }
+               }
+           }
+        }
     }
 
     if(sort == "Goals") {
